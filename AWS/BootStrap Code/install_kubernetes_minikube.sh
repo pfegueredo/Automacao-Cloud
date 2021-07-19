b3b9d99cf5fdc0ae059dae617ce7b13c433d5dfd
unknown <paulofegueredo@gmail.com>
2021-07-18 21:48:53 -0300

    Instalacao de servicos

---
+++ AWS/BootStrap Code/install_kubernetes_minikube.sh
@@ -0,0 +1,4 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_1.6.2.deb && dpkg -i minikube_1.6.2.deb
+minikube start --vm-driver=virtualbox
+snap install kubectl --classic
+apt install virtualbox
